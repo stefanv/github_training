6390a4ecc466c55e5668c8d74253d6f88d134f68
Dong Huk Park <dong.huk.park@berkeley.edu>
2016-02-24 15:53:55 -0800

    added docstring

--- calc.py
+++ calc.py
@@ -8,4 +8,11 @@ def div(a,b):
         raise ValueError("You should not try to divide by zero!")
 
 def subtract(a, b):
+    """
+    Subtracts b from a.
+    >>> subtract(4, 1)
+    3
+    >>> subtract(2, 3)
+    -1
+    """
     return a - b
