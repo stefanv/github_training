230e3c382c03f960ef53b4b0240e0316fd703411
Jennifer Dai <jenniferdai@berkeley.edu>
2016-02-24 16:04:27 -0800

    zero division raise ValueError

--- calc.py
+++ calc.py
@@ -2,6 +2,8 @@ def add(a, b):
     return a + b
 
 def div(a,b):
-    if b == 0:
-	   return "zero division error"
-    return a/b
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError("You should not try to divide by zero!")
+        
