408d1e97564708aab114ebb9b723de1695f8e264
Shaun Singh <shaunsingh@Shauns-MacBook-Pro.local>
2016-02-24 16:00:11 -0800

    Add docstring, add slight new functionality

--- multiply.py
+++ multiply.py
@@ -1,2 +1,15 @@
-def multiply(a, b):
-    return a * b
+def multiply(a, b=None):
+    """Multiply two numbers.
+
+    Parameters
+    ----------
+    a : int or float 
+        First operand.
+    b : int or float (optional)
+        Second operand.
+    Returns
+    -------
+    output : int or float
+        Multiplication of parameters, or a*a if b not provided. 
+    """
+    return a * a if b is None else a * b
