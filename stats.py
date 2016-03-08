f53eb4bc1b09a88ad6c5e7721464b5a2e2684390
Stefan van der Walt <stefanv@berkeley.edu>
2016-03-07 16:41:39 -0800

    Add variance

--- stats.py
+++ stats.py
@@ -1,6 +1,18 @@
 """
 This is our brand new stats module
 """
+import numpy as np
+
 
 def mean(x):
     raise NotImplementedError()
+
+def var(x):
+    """Calculate variance of x.
+
+    Parameters
+    ----------
+    x : array_like
+        Input values.
+    """
+    return np.var(x)
